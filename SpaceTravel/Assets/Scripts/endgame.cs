036d00bf6074a5e5bc8e78df592c09b28a6bd2d9
BramasoleJM <bramasolejm030206@gmail.com>
2022-08-06 17:09:24 +0800

    start and level select

---
+++ SpaceTravel/Assets/Scripts/endgame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endgame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EndingGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
---
+++ SpaceTravel/Assets/Scripts/loadgame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class loadgame : MonoBehaviour
+{
+    public int levelNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadingGame()
+    {
+        SceneManager.LoadScene(levelNum);
+    }
+}
